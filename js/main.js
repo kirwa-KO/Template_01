86995dbd6b6043a0945b9ed654c24df24fed5366
root <root@localhost.localdomain>
2020-04-23 00:51:51 +0000

    add navbar/slider and pricing table

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+	'use strict';
+
+	var		windowHeight   = $(window).innerHeight(),
+			upperBarHeight = $('.upper-bar').innerHeight(),
+			navBarHeight   = $('.navbar').innerHeight();
+
+	$('.slider, .carousel-item').height(windowHeight - (upperBarHeight + navBarHeight));
+
+	// Feauters Shuffle
+	$('.feauters-work ul li').on('click', function () {
+		$(this).addClass('active').siblings().removeClass('active');
+		// console.log($(this).data('class'));
+		if ($(this).data('class') == '.all')
+		{
+			$('.shuffle-imgs .col-md').css('opacity', 1)
+		}
+		else
+		{
+			$('.shuffle-imgs .col-md').css('opacity', '.08');
+			$($(this).data('class')).parent().css('opacity', 1);
+		}
+	})
+});
+
